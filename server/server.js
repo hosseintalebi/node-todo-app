f3e6749dbcea6d7e2c99640aa19eab78ce8c2212
hosseintalebi <htalebi@hotmail.com>
2017-09-09 15:34:48 -0600

    Add query todo by id and test

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { ObjectID } = require('mongodb')
 
 // Local imports
 const { mongoose } = require('./db/mongoose')
@@ -30,6 +31,19 @@ app.get('/todos', (req, res) => {
   }, (err) => {
     res.status(400).send(err)
   })
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send()
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+    res.send({todo})
+  }).then((err) => res.status(400).send())
 })
 
 app.listen(3000, () => {
