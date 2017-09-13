3c63ca0f0f5ef52de9c1646da0ba105f488af945
hosseintalebi <htalebi@hotmail.com>
2017-09-12 21:51:12 -0600

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,22 @@ UserSchema.methods.generateAuthToken =  function () {
     return token;
   })
 }
+
+UserSchema.statics.findByToken = function (token) {
+  const User = this
+  let decoded;
+  try {
+      decoded = jwt.verify(token, 'abc123')
+  } catch (e) {
+    return Promise.reject()
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  })
+}
 // User model
 const User = mongoose.model('users', UserSchema)
 
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ require('./config/config')
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
+const { authenticate } = require('./middleware/authenticate')
 
 const app = express()
 
@@ -95,6 +96,10 @@ app.post('/users', (req, res) => {
   }).catch((err) => {
     res.status(400).send(err)
   })
+})
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
 })
 
 app.listen(port, () => {
